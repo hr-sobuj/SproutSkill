801f8e88b71cc136a7b52d0ad541a6083d1bfd15
Habibur Rahman Sobuj <sobujhd@gmail.com>
2022-12-30 18:54:31 +0600

    hero text change

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Marcellus', 'serif'],
+        marcellus: ['Marcellus', 'serif'],
+        play: ['Play', 'sans-serif'],
       },
     },
   },
